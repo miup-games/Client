ada273255c32950ac8159f25fcdee9196f4830ee
Diego Leyton <dleyton@Diegos-MacBook-Air.local>
2016-05-04 02:44:28 -0700

    Implementing cards logic

---
+++ Assets/MIUP/Domain/Cards/Models/CardModel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+
+namespace MIUP.GameName.Domain.CardModule 
+{
+	public class CardModel
+	{
+        #region PROPERTIES
+        /// <summary>
+        /// Gets the rank.
+        /// </summary>
+        /// <value>The rank.</value>
+		public CardDefinitions.CardRanks Rank { get; private set; }
+
+        /// <summary>
+        /// Gets the suit.
+        /// </summary>
+        /// <value>The suit.</value>
+        public CardDefinitions.CardSuits Suit { get; private set; }
+        #endregion
+
+        #region PUBLIC METHODS
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MIUP.GameName.Domain.CardModule.CardModel"/> class.
+        /// </summary>
+        /// <param name="rank">Rank.</param>
+        /// <param name="suit">Suit.</param>
+        public CardModel(CardDefinitions.CardRanks rank, CardDefinitions.CardSuits suit)
+        {
+            if( (rank == CardDefinitions.CardRanks.Joker && (suit != CardDefinitions.CardSuits.Black && suit != CardDefinitions.CardSuits.Red)) ||
+                (rank != CardDefinitions.CardRanks.Joker && (suit == CardDefinitions.CardSuits.Black || suit == CardDefinitions.CardSuits.Red)) )
+            {
+                throw new System.ArgumentException(string.Format("Inconcistent rank {0} and suit {1}", rank, suit));
+            }
+
+            this.Rank = rank;
+            this.Suit = suit;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents the current <see cref="MIUP.GameName.Domain.CardModule.CardModel"/>.
+        /// </summary>
+        /// <returns>A <see cref="System.String"/> that represents the current <see cref="MIUP.GameName.Domain.CardModule.CardModel"/>.</returns>
+        public override string ToString() {
+            return string.Format("{0} of {1}", 
+                this.Rank == CardDefinitions.CardRanks.Joker ? "Jok" : this.Rank.ToString(),
+                this.Suit.ToString());
+        }
+
+        /// <summary>
+        /// Determines whether this instance can match the specified otherCard.
+        /// </summary>
+        /// <returns><c>true</c> if this instance can match the specified otherCard; otherwise, <c>false</c>.</returns>
+        /// <param name="otherCard">Other card.</param>
+        public bool CanMatch(CardModel otherCard) {
+            //Joker case
+            if(this.Rank == CardDefinitions.CardRanks.Joker || otherCard.Rank == CardDefinitions.CardRanks.Joker) {
+                return true;
+            }
+
+            //Case A and 2
+            int dif = UnityEngine.Mathf.Abs(this.GetRankIndex() - otherCard.GetRankIndex());
+            if(dif == (CardDefinitions.RankCount - 1)) {
+                return true;
+            }
+
+            //Other cases
+            return dif == 1;
+        }
+        #endregion
+
+        #region PRIVATE METHODS
+        /// <summary>
+        /// Gets the index of the rank.
+        /// </summary>
+        /// <returns>The rank index.</returns>
+        private int GetRankIndex() {
+            return (int)this.Rank;
+        }
+        #endregion
+	}
+}
---
+++ Assets/MIUP/Domain/Cards/Tests/Editor/CardModelTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using MIUP.GameName.Domain.CardModule;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace MIUP.GameName.Domain.CardModule.Tests
+{
+    [TestFixture]
+    public class CardModelTests 
+    {
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Black)]
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Red)]
+        [TestCase(CardDefinitions.CardRanks.CA, CardDefinitions.CardSuits.Clubs)]
+        [TestCase(CardDefinitions.CardRanks.C10, CardDefinitions.CardSuits.Diamonds)]
+        [TestCase(CardDefinitions.CardRanks.CJ, CardDefinitions.CardSuits.Hearts)]
+        [TestCase(CardDefinitions.CardRanks.C2, CardDefinitions.CardSuits.Spades)]
+        public void Constructor_ValidParameters_SetRightValue(CardDefinitions.CardRanks rank, CardDefinitions.CardSuits suit) 
+        {
+            CardModel cardModel = new CardModel(rank, suit);
+
+            Assert.AreEqual(cardModel.Rank, rank);
+            Assert.AreEqual(cardModel.Suit, suit);
+        }
+
+        [TestCase(CardDefinitions.CardRanks.C2, CardDefinitions.CardSuits.Red)]
+        [TestCase(CardDefinitions.CardRanks.CA, CardDefinitions.CardSuits.Black)]
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Clubs)]
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Diamonds)]
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Hearts)]
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Spades)]
+        public void Constructor_WrongParameters_ThrowsRightException(CardDefinitions.CardRanks rank, CardDefinitions.CardSuits suit) 
+        {
+            Assert.Throws<System.ArgumentException>(() => new CardModel(rank, suit));
+        }
+
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Black,
+            CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Red, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Black,
+            CardDefinitions.CardRanks.C3, CardDefinitions.CardSuits.Clubs, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.Joker, CardDefinitions.CardSuits.Red,
+            CardDefinitions.CardRanks.CA, CardDefinitions.CardSuits.Diamonds, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.CA, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.C2, CardDefinitions.CardSuits.Spades, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.CA, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.CK, CardDefinitions.CardSuits.Spades, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.C2, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.C3, CardDefinitions.CardSuits.Spades, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.C5, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.C6, CardDefinitions.CardSuits.Spades, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.CJ, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.C10, CardDefinitions.CardSuits.Spades, Result = true)]
+
+        [TestCase(CardDefinitions.CardRanks.C10, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.C10, CardDefinitions.CardSuits.Spades, Result = false)]
+
+        [TestCase(CardDefinitions.CardRanks.C5, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.C7, CardDefinitions.CardSuits.Spades, Result = false)]
+
+        [TestCase(CardDefinitions.CardRanks.CK, CardDefinitions.CardSuits.Diamonds,
+            CardDefinitions.CardRanks.C2, CardDefinitions.CardSuits.Spades, Result = false)]
+
+        public bool CanMatch_WithOtherValidCard_ReturnsRightResult(CardDefinitions.CardRanks rank1, CardDefinitions.CardSuits suit1,
+            CardDefinitions.CardRanks rank2, CardDefinitions.CardSuits suit2) 
+        {
+            CardModel cardModel1 = new CardModel(rank1, suit1);
+            CardModel cardModel2 = new CardModel(rank2, suit2);
+
+            Assert.AreEqual(cardModel1.CanMatch(cardModel2), cardModel2.CanMatch(cardModel1));
+            return cardModel1.CanMatch(cardModel2);
+        }
+    }
+}
